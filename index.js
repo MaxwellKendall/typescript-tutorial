c445852176bc7ee048477e0abc92a2f58de86a20
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-06-11 15:19:25 -0400

    showing off js-doc and @ts-check

---
+++ index.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+// the below is intentionally invalid to show the capabilities of js-doc
+
+/**
+ * @param {string} greeting
+ * @return {string}
+ */
+function greet(greeting) {
+    return 1;
+}
+
+greet(1);
