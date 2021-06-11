d1aec0f9819ee722d4bc19dbec5ccd98e92a269b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-06-11 10:12:32 -0400

    01 -- react config

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var React = require("react");
+var react_dom_1 = require("react-dom");
+var App = function () { return (React.createElement("h1", null, "Hello World")); };
+react_dom_1.render(React.createElement(App, null), document.getElementById('app'));
