e07d7844e90d997531df99ebf15a466f79ef0ec7
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-06-11 17:08:40 -0400

    06 -- using generics to handle unknown inputs/returns

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('Hello from TypeScript')
+// dont know what the input will be and therefore the return.
+// by convention, IDontKnow is T
+const fill = <IDontKnow extends unknown>(arr:any[], value:IDontKnow) => {
+    return arr.map(() => value);
+};
+
+const result = fill([1,2,3], 1);
+
+// result.map((v) => v.toUpperCase()); shows error
+result.map((v) => v.toExponential());
